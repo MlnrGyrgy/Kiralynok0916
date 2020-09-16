446bc527e759707982e9498f7ca0f0ea3b5c3cde
MlnrGyrgy <molnar.gyorgy.mrgr@gmail.com>
2020-09-16 08:29:35 +0200

    Add project files.

---
+++ Kiralynok0916/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kiralynok0916
+{
+    class Tabla
+    {
+        char[,] tomb = new char[8,8];
+        char UresCella;
+        int UresSorokSzama;
+        int UresOszlopokSzama;
+        public void Elhelyezes()
+        {
+
+        }
+        public void FajlbaIr()
+        {
+
+        }
+        public void Megjelenit()
+        {
+
+        }
+        public Tabla(char kter)
+        {
+
+        }
+        public void UresOszlop()
+        {
+
+        }
+        public void UresSor()
+        {
+
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
